898963320c31b7d96489aabc47ad445bb2c1bdd3
Przemysław Głębocki <glebocki.przemyslaw@gmail.com>
2019-05-22 12:05:45 +0200

    Changed signatures, made some methods private, some TODOs

--- BookDownloader.py
+++ BookDownloader.py
@@ -21,12 +21,13 @@ class BookDownloader:
     def start(self):
         self.delete_tmp_downloads()
         href_links = BookDownloader.get_all_book_links()
-        self.crawl_sub_pages(href_links)
+        self.__crawl_sub_pages(href_links)
 
-    def crawl_sub_pages(self, href_links):
+    def __crawl_sub_pages(self, href_links):
         for link in href_links:
+            # TODO: Low level operation: abstract it
             sub_page_link = BookDownloader.WEB_SITE_ADDRESS + link
-            self.crawl_to_sub_page(sub_page_link)
+            self.__crawl_to_sub_page(sub_page_link)
 
     @staticmethod
     def get_all_book_links():
@@ -38,33 +39,34 @@ class BookDownloader:
         href_links = selector.xpath('//a/@href').getall()
         return href_links
 
-    def crawl_to_sub_page(self, sub_link):
+    def __crawl_to_sub_page(self, sub_link):
         try:
             self.crawl_sub_page(sub_link)
         except Exception as exp:
             print('Error navigating to link : ', sub_link, ': ', exp)
 
-    def crawl_sub_page(self, sub_link):
+    def __crawl_sub_page(self, sub_link):
         response = requests.get(sub_link)
         if 200 == response.status_code:
             file_name = BookDownloader.get_download_file_name(response)
+            # TODO: Low level operations: abstract them
             src_url = sub_link + file_name
             destination_url = self.dest_dir_url + file_name
-            self.check_and_download(destination_url, src_url)
+            self.__check_and_download(src_url, destination_url)
 
-    def check_and_download(self, destination_url, src_url):
+    def __check_and_download(self, src_url, destination_url):
         print("Downloading " + src_url + " to: " + destination_url)
         if self.is_downloaded(destination_url):
             print("File already downloaded!")
             return
-        self.download(destination_url, src_url)
+        self.download(src_url, destination_url)
 
     @staticmethod
     def is_downloaded(destination_url) -> bool:
         return os.path.isfile(destination_url)
 
     @staticmethod
-    def download(destination_url, src_url):
+    def download(src_url, destination_url):
         wget.download(src_url, destination_url)
 
     @staticmethod
@@ -76,15 +78,15 @@ class BookDownloader:
 
     def delete_tmp_downloads(self):
         tmp_file_list = glob.glob(self.dest_dir_url + '*.tmp')
-        for tmpFilePath in tmp_file_list:
-            BookDownloader.delete_tmp_download(tmpFilePath)
+        for path in tmp_file_list:
+            BookDownloader.delete_tmp_download(path)
 
     @staticmethod
-    def delete_tmp_download(tmp_file_path):
+    def delete_tmp_download(path):
         try:
-            os.remove(tmp_file_path)
+            os.remove(path)
         except OSError as e:
-            print("Error while deleting file : ", tmp_file_path + " : " + e)
+            print("Error while deleting file : ", path + " : " + e)
 
 
 start = time.time()
